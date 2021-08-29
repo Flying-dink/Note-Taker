c3f4287b305f7ad644f14243e51e9beb3a782216
gail enstice <skyedoll@aol.com>
2021-08-29 13:38:08 -0400

    another html route

--- server.js
+++ server.js
@@ -85,6 +85,30 @@ router.delete("/notes/:id", (req, res) => {
   });
 });
 
+
+
+
+
+//Html Route
+
+const PORT = process.env.PORT || 3005;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes/index.js");
+const htmlRoutes = require("./routes/htmlRoutes/index.js");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(join(__dirname,"public")));
+
+
+app.use("/api", apiRoutes);
+app.use("/",htmlRoutes);
+app.listen(PORT, () => {
+console.log('App listening on port: ${PORT}');
+});
+
+
+//Html route
 router.get("/", (req,res) => {
 console.log("Hello");
 res.sendFile(join(_dirname, "../..public/notes.html"));
